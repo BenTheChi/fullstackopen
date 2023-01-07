ce1f8f8c1c264f65689b5c36a886edea4ba28c19
Ben Chi <benthechi@gmail.com>
2023-01-07 12:06:38 -0800

    Add part2 courseinfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,17 @@
+const Course = ({id, name, parts}) => {
+    let total = parts.reduce( (sum, part) => sum + part.exercises, 0);
+    
+    return (
+    <li key={id}>
+      <h1>{name}</h1>
+      <ul>
+        {parts.map(part => 
+            <li key={part.id}>{part.name} {part.exercises}</li>
+        )}
+      </ul>
+      <b>Total of {total} exercises</b>
+    </li>
+    )
+  }
+
+export default Course;
